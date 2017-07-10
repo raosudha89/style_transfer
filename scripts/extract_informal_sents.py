ae52f4ad5833182c357513847572fb2c840e34fa
Sudha Rao <raosudha89@gmail.com>
2017-07-10 13:30:03 +0000

    Extract informal sentences one per line along with csv

--- scripts/extract_informal_sents.py
+++ scripts/extract_informal_sents.py
@@ -5,18 +5,9 @@ from random import shuffle
 if __name__ == '__main__':
     answers_csv = open(sys.argv[1])
     answers_reader = csv.reader(answers_csv, delimiter='\t')
-    # existing_informal_sentences_reader = csv.reader(open(sys.argv[3]), delimiter=',')
-    # existing_informal_sentences = []
-    # i = 0
-    # for row in existing_informal_sentences_reader:
-    #     if i == 0:
-    #         i += 1
-    #         continue
-    #     s1, s2, s3, s4, s5 = row
-    #     s5 = s5.strip('\n')
-    #     existing_informal_sentences += [s1, s2, s3, s4, s5]
-    informal_sentences_file = open(sys.argv[2], 'w')
-    informal_sentences_file.write("sentence_1,sentence_2,sentence_3,sentence_4,sentence_5\n")
+    informal_sentences_csv_file = open(sys.argv[2], 'w')
+    informal_sentences_file = open(sys.argv[3], 'w')
+    informal_sentences_csv_file.write("sentence_1,sentence_2,sentence_3,sentence_4,sentence_5\n")
     scores = []
     i = 0
     informal = []
@@ -43,13 +34,14 @@ if __name__ == '__main__':
                 break
         for j in range(5):
             try:
-                line += '\"' + informal[i*5+j].decode('utf-8', 'ignore') + '\",'
-            except:
-                line += '\"' + informal[i*5+j].encode('utf-8') + '\",'
+		line += '\"' + informal[i*5+j].decode('utf-8', 'ignore') + '\",'
+	    except:
+		line += '\"' + informal[i*5+j].decode('utf-8') + '\",'
+	    informal_sentences_file.write(informal[i*5+j]+'\n')
         line = line[:-1] #to remove last comma
         if line[0] == '<':
             line = line[1:]
         if line[-1] == '>':
             line = line [:-1]
-        informal_sentences_file.write(line.encode('utf-8')+'\n')
+        informal_sentences_csv_file.write(line.encode('utf-8')+'\n')
             
