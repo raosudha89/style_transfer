423a38dc8f013eb74a0bedff827d978aa994de6d
Sudha Rao <sudharao@Sudhas-MacBook-Pro.local>
2017-07-12 16:34:07 -0400

    plot classifier results in bins

--- scripts/plot_classifier_results.py
+++ scripts/plot_classifier_results.py
@@ -1,6 +1,7 @@
 import sys, pdb
 import csv
 import matplotlib.pyplot as plt
+import numpy
 
 if __name__ == '__main__':
     results_csv = open(sys.argv[1])
@@ -8,8 +9,9 @@ if __name__ == '__main__':
     scores = []
     i = 0
     for row in results_reader:
-        score, sentence = row
+        splits = row
+        score = splits[0]
         scores.append(float(score))
-    plt.hist(scores, color='r')
-    plt.show()
+    plt.hist(numpy.array(scores), bins=[-3.0,-2.0,-1.0,0.0,1.0,2.0,3.0], color='y')
+    plt.savefig(sys.argv[2])
             
